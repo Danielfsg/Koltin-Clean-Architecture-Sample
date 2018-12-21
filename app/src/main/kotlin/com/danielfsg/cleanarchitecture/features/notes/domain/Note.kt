f45c454067715f1c6ed81200fe36a6b7207220c9
Danielfsg <dfsguer@gmail.com>
2018-12-21 09:43:19 +0000

    things

--- app/src/main/kotlin/com/danielfsg/cleanarchitecture/features/notes/domain/Note.kt
+++ app/src/main/kotlin/com/danielfsg/cleanarchitecture/features/notes/domain/Note.kt
@@ -1,14 +1,33 @@
 package com.danielfsg.cleanarchitecture.features.notes.domain
 
+import android.os.Parcel
+import com.danielfsg.cleanarchitecture.core.platform.KParcelable
+import com.danielfsg.cleanarchitecture.core.platform.parcelableCreator
 import com.danielfsg.cleanarchitecture.features.notes.data.NoteEntity
 import java.util.*
 
-data class Note(
-    val id: String? = null,
+data class Note (
+    val id: String,
     val title: String,
     val content: String,
     val createdAt: Date? = null,
     val updatedAt: Date? = null
-) {
+): KParcelable {
     fun toNoteEntity() = NoteEntity(title = title, content = content)
+
+
+    companion object {
+        @JvmField
+        val CREATOR = parcelableCreator(::Note)
+    }
+
+    constructor(parcel: Parcel) : this(parcel.readString(), parcel.readString(), parcel.readString())
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        with(dest) {
+            writeString(id)
+            writeString(title)
+            writeString(content)
+        }
+    }
 }
--- app/src/main/kotlin/com/danielfsg/cleanarchitecture/features/notes/presentation/note/AddEditNoteActivity.kt
+++ app/src/main/kotlin/com/danielfsg/cleanarchitecture/features/notes/presentation/note/AddEditNoteActivity.kt
@@ -1,8 +1,21 @@
 package com.danielfsg.cleanarchitecture.features.notes.presentation.note
 
+import android.content.Context
+import android.content.Intent
 import com.danielfsg.cleanarchitecture.core.platform.BaseActivity
-import com.danielfsg.cleanarchitecture.core.platform.BaseFragment
+import com.danielfsg.cleanarchitecture.features.notes.domain.Note
 
 class AddEditNoteActivity : BaseActivity() {
+
+    companion object {
+        private const val INTENT_EXTRA_PARAM_MOVIE = "com.danielfsg.INTENT_PARAM_MOVIE"
+
+        fun callingIntent(context: Context, note:Note): Intent {
+            val intent = Intent(context, AddEditNoteFragment::class.java)
+            intent.putExtra(INTENT_EXTRA_PARAM_MOVIE, note)
+            return intent
+        }
+    }
+
     override fun fragment() = AddEditNoteFragment()
 }
